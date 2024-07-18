a965dd7726664619c4e4a384314c9a0a99db5878
oaly <oaly@elm.sa>
2024-07-18 17:52:38 +0300

    adding http security with helmet

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const productRoutes = require('./routes/products');
 const userRoutes = require('./routes/users');
+const helmet = require('helmet');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
 
 app.use(bodyParser.json());
 
